0c0f908b6d18fa0bf68a6efb4ad175d0fb0d39f4
Estelle Barna <estelle.barna@epitech.eu>
2018-06-26 19:49:16 +0200

    database connection

--- main/app.js
+++ main/app.js
@@ -2,11 +2,16 @@ var express = require('express');
 var app = module.exports = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var config = require('./config/database');
-var User = require('./user.js');
+var config = require('../config/database');
+var User = require('../user.js');
 
 mongoose.connect(config.database);
 var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(callback){
+    console.log('connection succeeded');
+});
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({
@@ -22,11 +27,21 @@ app.start = function(){
 
 app.__setToken = function(req, res){
     //res.json({email:req.body.email});
-    var user = [
+    var data = [
         {
             email: req.body.email,
             token: 'toto'
         }
     ];
+    var user = new User({
+        email: data.email,
+        token: data.token
+    });
+    user.save(function(error){
+        console.log('user saved');
+        if(error){
+            console.error(error);
+        }
+    });
     res.json(user);
 }
