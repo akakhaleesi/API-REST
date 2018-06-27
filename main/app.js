735aa07f236ef0731c414227daf3be7acca8042b
Estelle Barna <estelle.barna@epitech.eu>
2018-06-27 17:52:33 +0200

    error gestion

--- main/app.js
+++ main/app.js
@@ -14,6 +14,7 @@ db.once('open', function(callback){
     console.log('connection succeeded');
 });
 
+app.use(bodyParser.text());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -28,82 +29,90 @@ app.start = function(){
 };
 
 app.__setToken = function(req, res){
-    var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-    var date = new Date();
-    var month = date.getUTCMonth() + 1;
-    var day = date.getUTCDate();
-    var year = date.getUTCFullYear();
+    if(req.body.email){
+        var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+        var date = new Date();
+        var month = date.getUTCMonth() + 1;
+        var day = date.getUTCDate();
+        var year = date.getUTCFullYear();
 
-    var newdate = year + "/" + month + "/" + day;
+        var newdate = year + "/" + month + "/" + day;
 
-    var user = new User({
-        email: req.body.email,
-        token: token,
-        count_words: 0,
-        date: newdate
-    });
-    user.save(function(error){
-        if(error){
-            console.error(error);
-            return res.json('User not saved');
-        }
-        else {
-            console.log('user saved');
-             return res.json('User saved');
-        }
-    });
-    return res.json(user);
+        var user = new User({
+            email: req.body.email,
+            token: token,
+            count_words: 0,
+            date: newdate
+        });
+        user.save(function(error){
+            if(error){
+                console.error(error);
+                return res.json('User not saved');
+            }
+            else {
+                console.log('user saved');
+                return res.json(user);
+            }
+        });
+    }
+    else {
+        return res.json('Enter your email');
+    }
 }
 
 app.__setText = function(req, res){
-    User.findOne({email: req.body.email, token: req.body.token})
-    .exec(function(err,doc){
-        if(doc){
-            if(req.body.text){
+    if(req.headers.email && req.headers.token){
+        User.findOne({email: req.headers.email, token: req.headers.token})
+        .exec(function(err,doc){
+            if(doc){
+                if(req.body){
+                    var date = new Date();
+                    var month = date.getUTCMonth() + 1;
+                    var day = date.getUTCDate();
+                    var year = date.getUTCFullYear();
 
-                var date = new Date();
-                var month = date.getUTCMonth() + 1;
-                var day = date.getUTCDate();
-                var year = date.getUTCFullYear();
+                    newdate = year + "/" + month + "/" + day;
 
-                newdate = year + "/" + month + "/" + day;
-
-                if(doc.date != newdate){
-                    doc.count_words = 0;
-                    doc.date = newdate;
-                }
-                if(doc.count_words === 80000){
-                    return res.status(402).send('Payment Required');
-                }
-                else {
-                    var text = req.body.text;
-                    var array = text.match(/.{1,80}/g);
-                    var final_text = '';
-
-                    var words = text.split(' ');
-                    var count = words.length;
-                    var inbetween = 80000 - doc.count_words;
-                    if(count > inbetween){
-                        return res.json('Only '+ inbetween +' words left for this day');
+                    if(doc.date != newdate){
+                        doc.count_words = 0;
+                        doc.date = newdate;
+                    }
+                    if(doc.count_words === 80000){
+                        return res.status(402).send('Payment Required');
                     }
                     else {
-                        var update = doc.count_words + count;
-                        doc.count_words = update;
-                        doc.save();
+                        var text = req.body;
+                        var array = text.match(/.{1,80}/g);
+                        var final_text = '';
 
-                        array.forEach(function(data){
-                            final_text += data + '\n';
-                        });
-                        return res.json(final_text);
+                        var words = text.split(' ');
+                        var count = words.length;
+                        var inbetween = 80000 - doc.count_words;
+                        if(count > inbetween){
+                            return res.json('Only '+ inbetween +' words left for this day');
+                        }
+                        else {
+                            var update = doc.count_words + count;
+                            doc.count_words = update;
+                            doc.save();
+
+                            array.forEach(function(data){
+                                final_text += data + '\n';
+                            });
+                            return res.json(final_text);
+                        }
                     }
+                }
+                else {
+                    return res.json('No text sent');
                 }
             }
             else {
-                return res.json('No text sent');
+                return res.json('User not found');
             }
-        }
-        else {
-            return res.json('User not found');
-        }
-    });
+        });
+    }
+    else {
+        return res.json('Missing email and/or token in header');
+    }
 }
